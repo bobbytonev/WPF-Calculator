fa811b4f2dbfb61858ee78e418354674c9c99351
Bobby Tonev <btonev@tu-sofia.bg>
2020-07-08 03:19:16 +0300

    Add project files.

---
+++ CalculatorWPF/Calcualtor.cs
@@ -0,0 +1,222 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Text.RegularExpressions;
+using System.Threading.Tasks;
+using System.Windows.Controls;
+
+namespace CalculatorWPF
+{
+    class Calcualtor
+    {
+
+       public TextBox box;
+      public  Calcualtor(TextBox box)
+        {
+            this.box = box;
+        }
+
+        private bool flag = false;
+        
+        public double eval(String equation)
+        {
+
+           equation = validate(equation);
+            if (equation.Equals(""))
+            {
+                box.Text = "Validation error";
+                return 0;
+            }
+            
+            List<String> splitedEquation = regexBuilder(equation, "\\s*(?:(?<![\\d-])-\\d[.]{0,1}\\d+|[+-\\/*()])|\\d+?[.]\\d|\\d\\s*");
+
+            
+
+            String[] tokens = Regex.Split(infixToPostFix(splitedEquation)," +");
+            Stack<Double> stack = new Stack<Double>();
+
+            for (int i = 0; i < tokens.Length; i++)
+            {
+                double number;
+
+                if (double.TryParse(tokens[i], out  number))
+                {
+
+                    stack.Push(number);
+
+                } else
+                {
+                    if (stack.Count < 2)
+                    {
+                        box.Text = "Expression error";
+                        return 0;
+                        
+                    }
+                    double a = stack.Pop();
+                    double b = stack.Pop();
+                    switch (tokens[i])
+                    {
+                        case "+":
+                          
+                            stack.Push(a + b);
+                            break;
+                        case "-":
+                          
+                            stack.Push(b - a);
+                            break;
+                        case "*":
+                          
+                            stack.Push(a * b);
+                            break;
+                        case "/":
+                             if (a != 0) {
+                                stack.Push(b / a);
+                              
+                            }
+                            else
+                            {
+                                return 0;
+                            }
+                                
+                           
+
+                            break;
+                    }
+                }
+            }
+
+            return stack.Pop();
+
+        }
+
+
+        public  List<String> regexBuilder(String rawData, String regex)
+        {
+
+            MatchCollection matchList = Regex.Matches(rawData, regex);
+
+
+            return matchList.Cast<Match>().Select(match => match.Value).ToList();
+        }
+
+
+
+        public String validate(String equation)
+        {
+            int brOpenBrakets=0;
+            int brCloseBrakets = 0;
+            
+            for (int i = 0; i < equation.Length; i++) { 
+                if(equation[i]=='(')
+                {
+                    brOpenBrakets++;
+                }
+                if (equation[i] == ')')
+                {
+                    brCloseBrakets++;
+                }
+            }
+            String newEquation = equation;
+            if (!Char.IsDigit(newEquation[newEquation.Length - 1])&&!newEquation[newEquation.Length - 1].Equals(')'))
+            {
+                newEquation=newEquation.Remove(newEquation.Length - 1); 
+            }
+           
+            int sum = brOpenBrakets - brCloseBrakets;
+            if (sum < 0)
+            {
+                return "";
+            }
+           
+            for (int i = 0; i < sum; i++)
+            {
+                newEquation += ")";
+            }
+
+
+            return newEquation;
+
+
+        }
+
+        public String infixToPostFix(List<String> token)
+        {
+            
+            StringBuilder postfix = new StringBuilder();
+
+            Stack<String> stack = new Stack<String>();
+
+            String operators = "+-*/";
+            int[] p = { 1, 1, 2, 2 };
+
+            for (int i = 0; i < token.Count; i++)
+            {
+
+                if (double.TryParse(token[i],out double param))
+               {
+                    postfix.Append(token[i]);
+                   
+               }
+                else
+                {
+                    
+                    if (token[i].Equals(")"))
+                    {
+                        while (stack.Any())
+                        {
+                            string sign = stack.Pop();
+                            flag = true;
+                            if (sign.Equals("("))
+                            {
+                                
+                                break;
+                            }
+                            postfix.Append(" "+sign);
+
+                        }
+                    }
+                    else
+                    {
+                        while (stack.Any())
+                        {
+
+                            string sign = stack.Pop();
+                            if (sign.Equals("(") || token[i].Equals("(")
+                               || p[operators.IndexOf(sign.ToCharArray()[0])] < p[operators.IndexOf(token[i])])
+                            {
+
+                                flag = true;
+                                stack.Push(sign);
+                                break;
+                            }
+                            postfix.Append(" "+sign);
+
+
+
+                        }
+                        if (!postfix.ToString().Equals(""))
+                        {
+                           
+                            postfix.Append(" ");
+                        }
+                        stack.Push(token[i]);
+                       
+
+                  }
+
+                }
+
+            }
+            while (stack.Any())
+            {
+                postfix.Append(" "+stack.Pop());
+            }
+
+            return postfix.ToString();
+
+        }
+    }
+
+
+}
---
+++ CalculatorWPF/MainWindow.xaml.cs
@@ -0,0 +1,114 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace CalculatorWPF
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+            
+        }
+
+       
+
+        private void TextBox_TextChanged(object sender, TextChangedEventArgs e)
+        {
+          
+        }
+
+        private void TextBox_TextChanged_1(object sender, TextChangedEventArgs e)
+        {
+
+        }
+
+        private void Button_Click(object sender, RoutedEventArgs e)
+        {
+
+        }
+
+        private void Button_Click_1(object sender, RoutedEventArgs e)
+        {
+
+        }
+
+        private void Button_Click_2(object sender, RoutedEventArgs e)
+        {
+            Calcualtor c = new Calcualtor(equals);
+
+
+
+           // string s = "(8-9)";
+            //String[] s1 =s.matches("\\s*(?:(?<![\\d-])-\\d|[-+\\/*()])|\\d+\\s*");
+
+            //  List<String> test = c.regexBuilder(s, "\\s*(?:(?<![\\d-])-\\d|[+-\\/*()])|\\d+?[.]\\d|\\d\\s*");
+            // MessageBox.Show(c.infixToPostFix(test));
+           //MessageBox.Show(c.validate("((5-9"));
+            
+           double result =c.eval(equals.Text);
+           hystory.Text += equals.Text+"="+result+"\n";
+
+            equals.Text = result + "";
+
+          
+
+        }
+
+        private void Button_Click_3(object sender, RoutedEventArgs e)
+        {
+            if (equals.Text.Length - 1 >= 0)
+            {
+                equals.Text = equals.Text.Remove(equals.Text.Length - 1);
+            }
+            
+        }
+
+      
+
+        private void command(object sender, RoutedEventArgs e)
+        {
+            Button command = sender as Button;
+
+            if (command == null)
+
+                return;
+
+
+            if (int.TryParse(command.Content.ToString(), out int number)|| command.Content.ToString().Equals("("))
+            {
+                if (equals.Text.Equals("0"))
+                {
+                    equals.Text = "";
+                }
+                
+            }
+                equals.Text += command.Content.ToString();
+                
+               
+            
+           
+           
+           
+          
+
+
+
+        }
+    }
+}
